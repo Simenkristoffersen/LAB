357566cc550944621b97f12341410c32858af9f8
Simen Kristoffersen <simenk3@gmai.com>
2013-09-19 23:04:26 +0200

    Create lab2.js

---
+++ lab2.js
@@ -0,0 +1,4 @@
+function selectDropdown(){
+var dropdownValue=document.getElementById("dropdown").value;
+document.getElementById("output").innerHTML="Du har slektert programmeringsspråket " + dropdownValue;
+}
